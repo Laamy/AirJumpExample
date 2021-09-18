296004615cd515dd28df1d4f54a69787bb2f15e2
Laamy <shade4keal@gmail.com>
2021-09-19 03:47:46 +1000

    more comments

--- BasicMCMUsage/Program.cs
+++ BasicMCMUsage/Program.cs
@@ -16,6 +16,9 @@ namespace BasicMCMUsage
             // Game > ClientInstance > LocalPlayer
             ulong clientInstance = MCM.baseEvaluatePointer(0x041457D8, new ulong[] { 0x0, 0x20 });
             ulong localPlayer = MCM.evaluatePointer(clientInstance, new ulong[] { 0xC8, 0x0 });
+            // all this means is Minecraft.Windows.exe+041457D8,0,20,C8,0(LocalPlayerPointer 1.17)
+            // we put 0x before a hex number to tell the programing language that its hex
+            // to fix this all you have to do is replace the broken pointer information
 
             // Game > ClientInstance > LocalPlayer > onGround
             ulong onGroundAddr = localPlayer + 0x1E0;
